40ff69d50dab6f07dbe9165991edba0b135752c1
Aldair-T <atorreq@fi.uba.ar>
2023-11-16 12:12:16 -0300

    Inicio del proyecto

---
+++ LocalDrive.py
@@ -0,0 +1,66 @@
+from service_drive import obtener_servicio as SERVICE_DRIVE
+from service_gmail import obtener_servicio as SERVICE_GMAIL
+from listar_archivos import *
+
+
+def menu() -> None:
+    """
+    Print del menu principal del programa
+    """
+    print("\n1) Listar archivos\n"
+          "2) Crear un archivo\n"
+          "3) Subir un archivo\n"
+          "4) Descargar un archivo\n"
+          "5) Sincronizar\n"
+          "6) Genera carpeta de una evaluacion\n"
+          "7) Actualizar entregas de alumnos via mail\n"
+          "8) Salir")
+    
+
+
+def opcion_valida(opcion: str) -> int:
+    """
+    Verificamos q la opcion sea correcta
+    """
+    while not opcion.isnumeric() or int(opcion) < 1 or int(opcion) > 8:
+        opcion = input("Ingrese una opcion correcta: ")
+    opcion = int(opcion)
+    return opcion
+
+
+
+def main() -> None:
+    """
+    Menu principal
+    """
+    acceso = True
+    while acceso:
+        print("\n\n\t\t\t\t BIENVENIDOS A DRIVEHUB \n")
+        menu()
+        opcion = input("Ingrese una opcion: ")
+        opcion = opcion_valida(opcion)
+        if opcion == 1:
+            listar_archivos()
+        elif opcion == 2:
+            pass
+            #creacion_archivos()
+        elif opcion == 3:
+            pass
+            #subir_archivos()
+        elif opcion == 4:
+            pass
+            #ingresar_carpeta_descarga()
+        elif opcion == 5:
+            pass
+            #sincronizar()
+        elif opcion == 6:
+            pass
+            #carpetas_encontradas()
+        elif opcion == 7:
+            pass
+            #enviar_mensaje()
+        elif opcion == 8:
+            acceso = False
+
+
+main()
---
+++ listar_archivos.py
@@ -0,0 +1,170 @@
+from service_drive import obtener_servicio as SERVICE_DRIVE
+from service_gmail import obtener_servicio as SERVICE_GMAIL
+from tabulate import tabulate
+import os, pathlib
+
+
+def opciones() -> None:
+    """
+    Tipos de repositorios, local o remoto
+    """
+    print("1) Archivos locales\n"
+          "2) Archivos remotos\n"
+          "3) Salir")
+
+
+
+def buscar_id_carpeta(nombre_carpeta) -> str:
+    """
+    Busca el id de la carpeta ingresada
+    pre: nombre de la carpeta a buscar
+    post: devuelve el id de la carpeta
+    """
+    id = ""
+    respuesta = SERVICE_DRIVE().files().list(q= "mimeType='application/vnd.google-apps.folder'" and f"name='{nombre_carpeta}'", fields="files(id, name, mimeType)").execute()
+    for valor in respuesta.values():
+        for i in valor:
+                id = i["id"]
+    return id
+
+
+
+def buscar_carpeta_drive() -> None:
+    """
+    Busca y lista una carpeta de DRIVE si existe
+    """
+    nombre_carpeta = input("Ingrese el nombre de la carpeta:")
+    print("\n")
+    id = buscar_id_carpeta(nombre_carpeta)
+    if id != "":
+        respuesta = SERVICE_DRIVE().files().list(q= f"parents = '{id}'", fields="files(id, name, mimeType)").execute()
+        listar_archivos_drive(respuesta)
+    else:
+        print("No se encontro la carpeta")
+
+
+
+def listar_archivos_drive(archivos) -> None:
+    """
+    Lista los archivos del parametro pasado
+    pre: archivos a listar
+    """
+    if not archivos:
+        print('No files found.')
+    else:
+        rows = []
+        for item in archivos.values():
+            for documento in item:
+                id = documento["id"]
+                name = documento["name"]
+                mime_type = documento["mimeType"]
+                rows.append((id, name, mime_type))
+        print("Files:")
+        # convert to a human readable table
+        table = tabulate(rows, headers=["ID", "Name", "Type"])
+        print(table)
+
+
+
+def repo_remoto() -> None:
+    """
+    Listamos todas los archivos y carpetas de drive
+    """
+    print("\nREPOSITORIO REMOTO:\n")
+    acceso = True
+    respuesta = SERVICE_DRIVE().files().list(fields="files(id, name, mimeType)").execute()
+    listar_archivos_drive(respuesta)
+    while acceso:
+        seguir = input("\nQueres buscar alguna carpeta? s/n: ")
+        if seguir == "s":
+            buscar_carpeta_drive()
+        elif seguir == "n":
+            acceso = False
+        else:
+            print("Ingrese una respuesta correcta")
+                           
+
+
+def volver(ruta) -> None:
+    """
+    Vuelve un paso atras de la ruta pasada como parametro
+    pre: ruta de la carpeta
+    post: ruta de la carpeta con un paso atras
+    """
+    if ruta != pathlib.Path.home():
+        ruta = ruta.parent
+        listar_archivos_local(ruta)
+    else:
+        print("Esta es la carpeta raiz")
+    return ruta
+
+
+
+def buscar_carpeta_local(ruta) -> None:
+    """
+    Busca y lista una carpeta del DIRECTORIO si existe
+    pre: ruta anterior
+    post: ruta con la carpeta buscada 
+    """
+    carpeta = input("Ingrese nombre de la carpeta: ")
+    ruta = pathlib.Path(ruta, carpeta)
+    if os.path.isdir(ruta):
+        listar_archivos_local(ruta)
+    else:
+        print("La carpeta no existe o es un archivo")
+        ruta = ruta.parent
+        buscar_carpeta_local(ruta)
+    return ruta
+
+
+
+def listar_archivos_local(ruta) -> None:
+    """
+    Funcion itera y lista los archivos
+    pre: ruta a listar
+    """
+    print("\n")
+    for archivo in os.listdir(ruta):
+        print(f"- {archivo}")    
+
+
+
+def repo_local() -> None:
+    """
+    Lista los archivos de la ruta base
+    """
+    print("\nREPOSITORIO LOCAL:")
+    acceso = True
+    ruta = pathlib.Path.home()
+    listar_archivos_local(ruta)
+    while acceso:
+        print("\n1)Buscar carpeta\n2)Volver\n3)Salir\n")
+        opcion = input("Ingrese una opcion: ")
+        while not opcion.isnumeric() or int(opcion) < 1 or int(opcion) > 3:
+            opcion = input("Ingrese una opcion correcta: ")
+        if int(opcion) == 1:
+            ruta = buscar_carpeta_local(ruta)
+        elif int(opcion) == 2:
+            ruta = volver(ruta)
+        elif int(opcion) == 3:
+            acceso = False
+
+
+
+def listar_archivos() -> None:
+    """
+    Elige si lista el repositorio local o remoto
+    """
+    acceso = True
+    while acceso:
+        print("\n\t\t\t\t LISTAR ARCHIVOS\n")
+        opciones()
+        opcion = input("Elija una opcion: ")
+        while not opcion.isnumeric() or int(opcion) < 1 or int(opcion) > 3:
+            opcion = input("Ingrese una opcion correcta: ")
+        if int(opcion) == 1:
+            repo_local()
+        elif int(opcion) == 2:
+            repo_remoto()
+        elif int(opcion) == 3:
+            acceso = False
